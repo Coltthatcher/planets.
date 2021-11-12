b4f87793db76a58236cd8fc80a6ba5f93f4da1eb
Colt Thatcher <colt.thatcher303@gmail.com>
2021-11-12 11:43:40 -0800

    run second bad test

--- __tests__/jupitor.test.js
+++ __tests__/jupitor.test.js
@@ -7,9 +7,13 @@ describe('Jupiter', () => {
     
     test('it should create a new jupiter object with a year value of 11.86', () => {
       const newJupiter = new Jupiter();
-      expect(newJupiter.yearValue).toEqual(11.86)
-      
+      expect(newJupiter.yearValue).toEqual(11.86)  
     });
-    
+
+    test('it should return your age in jupiter years', () => {
+        const newJupiter = new Jupiter(25);
+        newJupiter.calculateAge();
+        expect(newJupiter.jupiterAge).toEqual(2); 
+      });
 });
 
