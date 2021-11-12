c96339a48c17af6cd9c157ffa7e0d1b7788374e9
Colt Thatcher <colt.thatcher303@gmail.com>
2021-11-12 14:24:46 -0800

    run passing test for mars constructor

--- __tests__/mars.test.js
+++ __tests__/mars.test.js
@@ -3,7 +3,7 @@ import Mars from "../src/js/mars";
 
 describe('Mars', ()=> {
 
-  test('it should create a new mars object with the year value of 1.88', () => {
+  test('it should make a new mars object with the year value of 1.88', () => {
     const newMars = new Mars();
     expect(newMars.yearValue).toEqual(1.88)
   });
