e104b029d104b322e5a4ab21a91101362955ac75
Colt Thatcher <colt.thatcher303@gmail.com>
2021-11-12 11:55:52 -0800

    pass second test

--- src/js/jupiter.js
+++ src/js/jupiter.js
@@ -5,4 +5,10 @@ export default class Jupiter{
     this.yearValue = 11.86;
     this.jupiterAge = 0;
   }
+  calculateAge(){
+    return this.jupiterAge += Math.floor((this.age / this.yearValue))
+  }
 }
+
+
+
