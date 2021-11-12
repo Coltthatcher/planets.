5775b4d15bee93782c27fba83b03be86b1b12d4c
Colt Thatcher <colt.thatcher303@gmail.com>
2021-11-12 11:17:02 -0800

    build constructor for jupiter so good test can be run

--- __tests__/jupitor.test.js
+++ __tests__/jupitor.test.js
@@ -1,11 +1,13 @@
 import { exportAllDeclaration } from "@babel/types";
-import { TestScheduler } from "jest"
+import { TestScheduler } from "jest";
+import Jupiter from '../src/js/jupiter.js';
+
 
 describe('Jupiter', () => {
     
-    test('it should create a new jupiter object with a value of 11.86', () => {
-      const newJupiter  = new Jupiter() ;
-      expect(newJupiter).toEqual(11.86)
+    test('it should create a new jupiter object with a year value of 11.86', () => {
+      const newJupiter = new Jupiter();
+      expect(newJupiter.yearValue).toEqual(11.86)
       
     });
     
---
+++ src/js/jupiter.js
@@ -0,0 +1,8 @@
+export default class Jupiter{
+  constructor(age, lifeExpect){
+    this.age = age;
+    this.lifeExpect = lifeExpect;
+    this.yearValue = 11.86;
+    this.jupiterAge = 0;
+  }
+}
