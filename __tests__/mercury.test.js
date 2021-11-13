a9213724b1b585e68f6ec4c6f649c8ee475fb0a1
Colt Thatcher <colt.thatcher303@gmail.com>
2021-11-13 10:20:16 -0800

    pass 13th test

--- __tests__/mercury.test.js
+++ __tests__/mercury.test.js
@@ -1,14 +1,14 @@
 import { exportAllDeclaration } from "@babel/types";
 import { TestScheduler } from "jest"
 
-
+import Mercury from '../src/js/mercury.js';
 
 
 describe('Mercury', () => {
 
   test('it should create a new mercury object with a year value of .24', () => {
     const newMercury = new Mercury();
-    expect(newMercury.yearvalue).toEqual(.24)
+    expect(newMercury.yearValue).toEqual(.24)
 
   })
 })
---
+++ src/js/mercury.js
@@ -0,0 +1,8 @@
+export default class Mercury{
+  constructor(age,lifeExpect){
+    this.age = age;
+    this.lifeExpect = lifeExpect;
+    this.yearValue = .24;
+    this.mercuryAge = 0;
+  }
+}
