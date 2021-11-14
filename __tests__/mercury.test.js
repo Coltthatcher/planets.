47babbe4c9874f7d7fd2fbb4678779262c75baf9
Colt Thatcher <colt.thatcher303@gmail.com>
2021-11-14 14:01:42 -0800

    pass 16th test

--- __tests__/mercury.test.js
+++ __tests__/mercury.test.js
@@ -20,16 +20,16 @@ describe('Mercury', () => {
   });
 
   test('it should determine how many years the user has left to live', () => {
-    const newMercury = new Mercury(25, 100);
+    const newMercury = new Mercury(25, 105);
     newMercury.calculateAge();
     newMercury.yearsLeft();
-    expect(newMercury.yearsLeft()).toEqual(4);
+    expect(newMercury.yearsLeft()).toEqual(1);
   });
 
   test('will determine how many years past past life expectancy', () => {
     const newMercury = new Mercury(25, 1);
     newMercury.calculateAge();
     newMercury.yearsLeft();
-    expect(newMercury.yearsLeft()).toEqual(1)
+    expect(newMercury.yearsLeft()).toEqual(103)
   });
 });
